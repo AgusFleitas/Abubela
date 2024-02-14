4423ad6767930640fea91d023315aa1b12099ca5
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-02-14 13:20:46 +0100

    Corrección de elementos en CSS

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -10,17 +10,17 @@ const Home = () => {
   return (
     <div className={style.homeContainer}>
       <div className={style.outImgContainer}>
-        <img src={outside} alt='outside view of A Bubela' />
+        <img src={outside} alt='outside view of A Bubela' className={style.outImg}/>
       </div>
-      <div className={style.mainText}>
-        <h3>¿Vienes a pasar un buen rato con nosotros?</h3>
-        <p>
+      <div className={style.mainTextContainer}>
+        <h3 className={style.mainTitle}>¿Vienes a pasar un buen rato con nosotros?</h3>
+        <p className={style.mainText}>
           Ya sea que estés buscando un lugar para tomar un descanso durante tu
           jornada o para disfrutar de una tarde llena de sabores, estamos aquí
           para hacer de tu experiencia una verdadera delicia.
         </p>
         <br></br>
-        <p>
+        <p className={style.mainText}>
           En A Bubela fusionamos lo mejor de dos mundos: la calidez y aroma del
           café recién hecho, y la frescura y diversidad de los refrescos
           acompañados de deliciosos pinchos.
@@ -41,12 +41,12 @@ const Home = () => {
       </div>
       <div className={style.bottomImages}>
         <div className={style.services}>
-          <a href='#'>
+          <a href='#' className={style.serviceLink}>
             <p className={style.serviceTitle}>PLATOS</p>
-            <img src={plates} alt='plates preview' />
+            <img src={plates} alt='plates preview' className={style.serviceImg}/>
             <div className={style.servicesHover}>
-              <h4>NUESTROS PLATOS</h4>
-              <p>
+              <h4 className={style.hoverTitle}>NUESTROS PLATOS</h4>
+              <p className={style.hoverText}>
                 Tenemos a tu disposición nuestro menú completo para que puedas
                 elegir tu plato favorito y ver su precio.
               </p>
@@ -54,25 +54,25 @@ const Home = () => {
           </a>
         </div>
         <div className={style.services}>
-          <a href='#'>
+          <a href='#' className={style.serviceLink}>
             <p className={style.serviceTitle}>EMPRESAS</p>
-            <img src={business} alt='business preview' />
+            <img src={business} alt='business preview' className={style.serviceImg}/>
             <div className={style.servicesHover}>
-              <h4>CENA PARA EMPRESAS O EVENTOS</h4>
-              <p>
-                ¿Necesitas organizar una cena para tu empresa? Podes realizar
+              <h4 className={style.hoverTitle}>CENA PARA EMPRESAS O EVENTOS</h4>
+              <p className={style.hoverText}>
+                ¿Necesitas organizar una cena para tu empresa? Puedes realizar
                 una reserva y consultar sobre la comida. ¡Tenemos menú completo!
               </p>
             </div>
           </a>
         </div>
         <div className={style.services}>
-          <a href='#'>
+          <a href='#' className={style.serviceLink}>
             <p className={style.serviceTitle}>CONTACTO</p>
-            <img src={contact} alt='contact preview' />
+            <img src={contact} alt='contact preview' className={style.serviceImg}/>
             <div className={style.servicesHover}>
-              <h4>CONTACTA CON NOSOTROS</h4>
-              <p>
+              <h4 className={style.hoverTitle}>CONTACTA CON NOSOTROS</h4>
+              <p className={style.hoverText}>
                 Puedes enviarnos un mensaje por WhatsApp, llamar a nuestro
                 teléfono o enviarnos un mail a través de nuestro formulario.
               </p>
