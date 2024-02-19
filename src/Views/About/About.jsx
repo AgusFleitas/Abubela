81fbc13ecf05e292f7529d9dbcc2871d89f283ae
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-02-19 16:33:56 +0100

    About terminado - Cambio de fondo

--- src/Views/About/About.jsx
+++ src/Views/About/About.jsx
@@ -1,25 +1,32 @@
 import family from "../../assets/family.png";
+import outside2 from "../../assets/outside2.jpg";
 
 import style from "./About.module.css";
 
 const About = () => {
   return (
     <div className={style.aboutContainer}>
-      <div className={style.imageContainer}>
-        <img src={family} alt='Los propietarios de A Bubela y su familia' />
-      </div>
       <div className={style.upperContainer}>
-        <h2 className={style.upperTitle}>¡Bienvenidos a Abubela!</h2>
-        <p className={style.upperText}>
-          Somos Candy Guede y Eulogio Gómez, los orgullosos propietarios de este
-          encantador local en la calle Santo Domingo de Ourense. Con décadas de
-          experiencia en el mundo de la hostelería, hemos visto cómo este
-          espacio ha sido hogar de diversos negocios a lo largo de los años,
-          cada uno dejando su huella en la comunidad. Sin embargo, tras el
-          cierre de la tapería Bekas este verano, nuestros hijos Ana, Luci y
-          Javier tuvieron una brillante idea: ¿por qué no abrir algo nosotros
-          mismos?
-        </p>
+        <div className={style.imageContainer}>
+          <img
+            className={style.upperImg}
+            src={family}
+            alt='Los propietarios de A Bubela y su familia'
+          />
+        </div>
+        <div className={style.upperTextContainer}>
+          <h2 className={style.upperTitle}>¡Bienvenidos a Abubela!</h2>
+          <p className={style.upperText}>
+            Somos Candy Guede y Eulogio Gómez, los orgullosos propietarios de
+            este encantador local en la calle Santo Domingo de Ourense. Con
+            décadas de experiencia en el mundo de la hostelería, hemos visto
+            cómo este espacio ha sido hogar de diversos negocios a lo largo de
+            los años, cada uno dejando su huella en la comunidad. Sin embargo,
+            tras el cierre de la tapería Bekas, nuestros hijos Ana, Luci y
+            Javier tuvieron una brillante idea: ¿por qué no abrir algo nosotros
+            mismos?
+          </p>
+        </div>
       </div>
       <div className={style.lowerContainer}>
         <p className={style.lowerText}>
@@ -27,20 +34,19 @@ const About = () => {
           como un espacio donde disfrutar de buena comida y mejor compañía desde
           las nueve de la mañana hasta la una de la madrugada. Nos
           especializamos en desayunos deliciosos y platos para compartir que
-          harán las delicias de todos los paladares. En nuestra carta,
-          encontrarás favoritos que estamos seguros se convertirán en
-          imprescindibles, como los langostinos rebozados en kikos con salsa al
-          curry o la tosta de pulpo con parmentier y aceite de pimentón de La
-          Vera. También ofrecemos baos y croquetas elaboradas con ingredientes
-          frescos del día, así como ensaladas gourmet para satisfacer cualquier
-          antojo.
+          harán las delicias de todos los paladares. Estamos emocionados de
+          abrir nuestras puertas y compartir nuestra pasión por la buena comida
+          con vosotros. A Bubela es más que un simple local; es un lugar donde
+          la comida se convierte en experiencias memorables y donde cada cliente
+          es recibido con una sonrisa.
         </p>
-        <p className={style.lowerText}>
-          Estamos emocionados de abrir nuestras puertas y compartir nuestra
-          pasión por la buena comida con vosotros. A Bubela es más que un simple
-          local; es un lugar donde la comida se convierte en experiencias
-          memorables y donde cada cliente es recibido con una sonrisa.
-        </p>
+        <div className={style.imageLowContainer}>
+          <img
+            className={style.lowerImg}
+            src={outside2}
+            alt='Vista desde fuera de A Bubela'
+          />
+        </div>
       </div>
     </div>
   );
