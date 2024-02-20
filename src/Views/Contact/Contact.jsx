200388555b6f3781d9c448cc673c10e288d5f5fc
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-02-20 17:34:54 +0100

    Contact terminado - centrado

--- src/Views/Contact/Contact.jsx
+++ src/Views/Contact/Contact.jsx
@@ -1,31 +1,28 @@
-import inside from "../..//assets/inside.jpg";
-
 import style from "./Contact.module.css";
 
 const Contact = () => {
   return (
     <div className={style.contactContainer}>
-      <div className={style.imageContainer}>
-        <img
-          className={style.insideImg}
-          src={inside}
-          alt='Inside of A Bubela'
-        />
-      </div>
       <div className={style.rightContainer}>
         <h1 className={style.rightTitle}>Contáctanos</h1>
         <div className={style.infoContainer}>
           <div className={style.infoSubContainer}>
             <span className={style.infoTitle}>
-              <box-icon name='time' color='#f19409' size='md'></box-icon> NUESTRO HORARIO:
+              <box-icon name='time' color='#f19409' size='md'></box-icon>
+              NUESTRO HORARIO:
             </span>
-            <span className={style.infoText}>Lun a Jue de 09:00 am a 00:00 am.</span>
-            <span className={style.infoText}>Vie y Sab de 10:00 am a 01:00 am.</span>
+            <span className={style.infoText}>
+              Lun a Jue de 09:00 am a 00:00 am.
+            </span>
+            <span className={style.infoText}>
+              Vie y Sab de 10:00 am a 01:00 am.
+            </span>
             <span className={style.infoText}>Domingos CERRADO.</span>
           </div>
           <div className={style.infoSubContainer}>
             <span className={style.infoTitle}>
-              <box-icon name='map' color='#f19409' size='md'></box-icon> DIRECCION:
+              <box-icon name='map' color='#f19409' size='md'></box-icon>
+              DIRECCIÓN:
             </span>
             <span className={style.infoText}>
               Rúa Santo Domingo, 15, 32003 Ourense.
@@ -33,11 +30,17 @@ const Contact = () => {
           </div>
           <div className={style.infoSubContainer}>
             <span className={style.infoTitle}>
-              <box-icon name='phone' color='#f19409' size='md'></box-icon> TELEFONO:
+              <box-icon name='phone' color='#f19409' size='md'></box-icon>
+              TELÉFONO:
             </span>
             <span className={style.infoText}>+34 659 52 84 36</span>
           </div>
         </div>
+        <iframe
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2949.205449936247!2d-7.8659346234979735!3d42.33814293648545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd2fffbc5f88ecf5%3A0xd964bf1af4175b1b!2sA%20Bubela%20Snack%20Bar!5e0!3m2!1ses!2ses!4v1708370838324!5m2!1ses!2ses'
+          loading='lazy'
+          referrerPolicy='no-referrer-when-downgrade'
+        ></iframe>
       </div>
     </div>
   );
